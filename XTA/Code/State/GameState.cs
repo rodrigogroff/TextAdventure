bbb83865e173a543ab0d6b3cfbd1129aa43be4b9
rodrigo groff <rodrigo.groff_ext@interplayers.com.br>
2023-08-01 11:59:24 -0300

    ajustes

--- XTA/Code/State/GameState.cs
+++ XTA/Code/State/GameState.cs
@@ -51,6 +51,17 @@ namespace XTA.Code.State
                 case Keys.Y: return shift ? 'Y' : 'y';
                 case Keys.Z: return shift ? 'Z' : 'z';
 
+                case Keys.NumPad0: return '0';
+                case Keys.NumPad1: return '1';
+                case Keys.NumPad2: return '2';
+                case Keys.NumPad3: return '3';
+                case Keys.NumPad4: return '4';
+                case Keys.NumPad5: return '5';
+                case Keys.NumPad6: return '6';
+                case Keys.NumPad7: return '7';
+                case Keys.NumPad8: return '8';
+                case Keys.NumPad9: return '9';
+                        
                 case Keys.D0: return shift ? '0' : '0';
                 case Keys.D1: return shift ? '1' : '1';
                 case Keys.D2: return shift ? '2' : '2';
