3c4eb67c9ab519df90a171f4378a5c640fae9207
rodrigo groff <rodrigo.groff@vortigo.com.br>
2024-03-04 13:22:29 -0300

    ajustes

--- TextAdventure/obj/Debug/net6.0/TextAdventure.AssemblyInfo.cs
+++ TextAdventure/obj/Debug/net6.0/TextAdventure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextAdventure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+920fb7dedcf0f11c8e7f22a3a3cf3864f8b54d53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb88e328a6fd5374675434eda9ef50afe64fc27b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextAdventure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextAdventure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
